64defc429fdaa8c3000d9eb59df4ce21d56336c4
Caagr98 <Caagr98@users.noreply.github.com>
2016-02-07 12:27:34 +0100

    Cleanup, better color

--- Lemmiwinks/Forward.java
+++ Lemmiwinks/Forward.java
@@ -4,33 +4,36 @@ import hockey.api.*;
 
 public class Forward extends BasePlayer {
     int idx;
-    IPlayer other;
 
     public Forward(int i) {
     	idx = i;
 	}
-    @Override public void init() {
-    	other = getPlayer(6-idx);
-    }
 
 	public int getNumber() { return 0; }
     public String getName() { return "Forward"; }
-    @Override public boolean isLeftHanded() {
-    	return idx == 0;
-    }
+    @Override public boolean isLeftHanded() { return idx == 0; }
     public void step() {
     	setAimOnStick(true);
     	if(hasPuck() && Util.dist(this, getPlayer(5)) < 1000) {
-    		shoot(getPlayer(5), 700);
     		setMessage("Passing");
+    		shoot(getPlayer(5), 700);
     	} else if(hasPuck()) {
     		setMessage("Getting into position");
+    		skateTo(getPlayer(5));
     	} else {
-    		skateTo(getPuck());
     		setMessage("Chasing puck");
+    		skateTo(getPuck());
     	}
     }
 	private void skateTo(IObject goal) {
+		int totDist = 0;
+		for(int i = 0; i < 12; i++) {
+			double dst = Util.dist(this, getPlayer(i));
+			if(dst != 0) totDist += (int)(1000/dst);
+		}
+		totDist *= totDist;
+		totDist /= 100;
+		setMessage(""+totDist);
 		int dist = (int)Util.dist(this, goal);
 		int spd = 1000;
 		if(dist < 200) spd = 300;
