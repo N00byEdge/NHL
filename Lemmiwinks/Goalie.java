bd909d8155d36aaf4999ae466e9caa7da176d6bc
Johan Vikstrom <tritzium@gmail.com>
2016-02-07 11:02:43 +0100

    goalie now stands at the goal

--- Lemmiwinks/Goalie.java
+++ Lemmiwinks/Goalie.java
@@ -33,7 +33,7 @@ public class Goalie extends GoalKeeper {
     	if(random == null){
     		random = new Random();
     	}
-    	skate(GOAL_POSITION.getX() + 50, GOAL_POSITION.getY(), 200);
+    	//skate(GOAL_POSITION.getX() + 50, GOAL_POSITION.getY(), 200);
     	
     	turn(getPuck(), MAX_TURN_SPEED);
     	if(hasPuck()){
@@ -48,11 +48,28 @@ public class Goalie extends GoalKeeper {
         	skate(GOAL_POSITION.getX() + 50, GOAL_POSITION.getY(), 200);
     	}
     }
+    public int getGoalX(){
+    	return -2600;
+    }
+    public int getGoalY(){
+    	return 0;
+    }
     public void facePuck(){
-    	//turn(getPuck(), MAX_TURN_SPEED);
+    	turn(getPuck(), MAX_TURN_SPEED);
     	//Glide in a clever way here
     	
     	//skate(GOAL_POSITION.getX() + 50, GOAL_POSITION.getY(), 200);
+    	//skate(-2550, 0, 200);
+    	//turn(getPuck(), MAX_TURN_SPEED);
+    	//glide((getY() - getPuck().getY()));
+    	int xdir = getGoalX() - getPuck().getX(), ydir = getGoalY() - getPuck().getY(), size = Math.max((int)Math.sqrt(xdir * xdir + ydir * ydir), 1);
+    	int normx = xdir / size, normy = ydir / size;
+    	int wantedx = getGoalX() + 50 * normx, wantedy = getGoalY() + 50 * normy;
+    	int speed = ((int)Math.sqrt(Math.pow((wantedx - getX()), 2) + Math.pow(wantedy - getY(), 2))) / 4;
+    	skate(wantedx, wantedy, speed);
+    	setDebugPoint(wantedx, wantedy, java.awt.Color.GREEN);
+    	showDebugPoint(true);
+    	/*
     	int yPos = GOAL_POSITION.getY();
     	if(getPuck().getY() < 0){
     		yPos = Math.max(getPuck().getY(), GOAL_POSITION.getY() - 100);
@@ -60,6 +77,8 @@ public class Goalie extends GoalKeeper {
     		yPos = Math.min(getPuck().getY(), GOAL_POSITION.getY() + 100);
     	}
     	skate(GOAL_POSITION.getX() + 50, yPos, 200);
+    	setDebugPoint(GOAL_POSITION.getX() + 50, yPos, java.awt.Color.GREEN);
+    	showDebugPoint(true);*/
     	//glide(getY() - getPuck().getY());
 /*		int xdir = getPuck().getX() - GOAL_POSITION.getX(), ydir = getPuck().getY() - GOAL_POSITION.getY(), size = Math.max((int)Math.sqrt((double)xdir*xdir + ydir * ydir), 1);
 		int normalizedx = xdir / size, normalizedy = ydir / size;
