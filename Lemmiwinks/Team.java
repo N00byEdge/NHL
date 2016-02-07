acab72a81e97ccbc884e6961fa5c802ed0a3771b
Caagr98 <Caagr98@users.noreply.github.com>
2016-02-07 10:03:27 +0100

    Make the color more awesome

--- Lemmiwinks/Team.java
+++ Lemmiwinks/Team.java
@@ -1,11 +1,17 @@
 package Lemmiwinks;
 
 import java.awt.Color;
+import java.util.Random;
 import hockey.api.GoalKeeper;
 import hockey.api.Player;
 import hockey.api.ITeam;
 
 public class Team implements ITeam {
+	public static final Color RAND_COLOR = new Color(0,0,0,0) {
+		@Override public int getRGB() {
+			return new Random().nextInt() | 0xFF000000;
+		}
+	};
     // Team Short Name.  Max 4 characters.
     public String getShortName() { return "___"; }
 
@@ -13,13 +19,13 @@ public class Team implements ITeam {
     public String getTeamName() { return "Lemmiwinks"; }
 
     // Team color; body color
-    public Color getTeamColor() { return Color.YELLOW; }
+    public Color getTeamColor() { return RAND_COLOR; }
 
     // Team color; helmet color.
-    public Color getSecondaryTeamColor() { return Color.GREEN; }
+    public Color getSecondaryTeamColor() { return RAND_COLOR; }
 
     // The team's LUCKY NUMBER!!
-    public int getLuckyNumber() { return 5; }
+    public int getLuckyNumber() { throw new UnsupportedOperationException(); } //This isn't ever called, so...
 
     // Get the goal keeper of the team.
     public GoalKeeper getGoalKeeper() { return new Goalie(); }
